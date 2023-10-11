c38a071b28d875fd556e9e49071b0b363115e00f
burak <msburaks@gmail.com>
2023-10-11 08:19:58 +0300

    add attackFonction props

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,8 @@
 const ATTACK_VALUE=10;
+const STRONG_ATTACK_VALUE=17;
+const MONSTER_ATTACK_VALUE= 14;
+
+
 
 let chosenMaxLife =100;
 let currentMonsterHealt = chosenMaxLife;
@@ -6,8 +10,31 @@ let currentPlayerHealt =chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler(){
-    const damage =dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode){
+    let maxDamage;
+    if(mode === 'ATTACK'){
+        maxDamage=ATTACK_VALUE;
+    }else if(mode ==='STRONG_ATTACK'){
+        maxDamage= STRONG_ATTACK_VALUE;
+    }
+    const damage =dealMonsterDamage(maxDamage);
     currentMonsterHealt -= damage;
+    const playerDamage =dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealt -= playerDamage;
+    if(currentMonsterHealt<=0 && currentPlayerHealt>0){
+        alert('You Won');
+    }else if(currentPlayerHealt<=0 && currentMonsterHealt>0){
+        alert('You lost');
+    }else if(currentPlayerHealt<=0 && currentMonsterHealt<=0){
+        alert('You have a draw!');
+    }
+}
+
+function attackHandler(){
+    attackMonster('ATTACK');
+}
+function strongAttackHandler(){
+    attackMonster('STRONG_ATTACK');
 }
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click',strongAttackHandler);
